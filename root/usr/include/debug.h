9c5ad9cdd93a35056d485c682d4a59fa3fe6d369
Olivier de Schaetzen <olivier4@telenet.be>
2016-01-02 18:16:18 +0100

    updated makefile

--- root/usr/include/debug.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#define UNUSED(X) (void)x
--- root/usr/include/interruptHandlers.h
+++
@@ -1,6 +0,0 @@
- #pragma once
- #include "types.h"
- #include "defines.h"
- 
- void intHandlerUndefined(u32 interrupt);
- void intHandlerKeyboard(u32 interrupt);
